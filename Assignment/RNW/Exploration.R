0bcd5fd3192b44ce5c29b885219e4a30c226c733
Lewis <lewgoesb00m@gmail.com>
2024-04-19 12:55:08 +0100

    Folder restructure and RNW work

---
+++ Assignment/RNW/Exploration.R
@@ -0,0 +1,16 @@
+# Open the csv file and import it into R, converting string data to factors.
+data <- read.csv('googleplaystore.csv', stringsAsFactors = T)
+summary(data)
+
+# Replace all instances of 'NA' as a string or a blank string "" to NA.
+# The strings 'NA' or " " and NA are two different things to R, and only the
+# non-string NA is detected by functions like is.na().
+data[(data == 'NA' | data == "" | data == "NaN")] <- NA
+
+# Output a summary of the data.
+summary(data)
+
+# Identify rows containing NA.
+naRows <- data[rowSums(is.na(data)) > 0,]
+# 1469 with no ratings, 8 with no current version, 4 with neither
+nrow(naRows)
