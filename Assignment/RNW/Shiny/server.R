b6791dbf01aa0156deeb36caeb6dff6c23496e29
Lewis <lewgoesb00m@gmail.com>
2024-05-16 22:30:30 +0100

    Done pending proofread

--- Assignment/RNW/Shiny/server.R
+++ Assignment/RNW/Shiny/server.R
@@ -1,23 +1,69 @@
 library(shiny)
+library(tidyverse)
+library(ggplot2)
+library(plotly)
 
-# Shiny requires a client-server setup, with a file for each.
 # This file declares the server backend, which processes client inputs.
 
+dataDf <- read.csv("C:/Users/Lewis/DataspellProjects/DataVis/Assignment/RNW/data/wrangledData.csv")
+
 shinyServer(function(input, output) {
+  # Even though it is numeric, the app refuses to run unless I respecify that.
+  dataDf$numSize <- as.numeric(dataDf$numSize)
 
-  # renderPlot is called when the client updates its input (I think),
-  # so by including our
-  # graph production within this function, it will be updated dynamically.
+  # Reviews has some values that cause the Y axis to inflate massively.
+  tmpData <- dataDf %>% filter(Reviews < 4000000) 
 
-  output$distPlot <- renderPlot({
-    dist <- rnorm(input$obs)
 
-    # UI declares that it's sending var 'obs' to server,
-    # so it doesn't need to be declared as the server can interpret that via
-    # input$obs, which gets the obs column from the input frame. If that
-    # doesn't exist through misspelling or some other reason, then you'll
-    # get an error.
+  # Graphs for the histograms page.
+  output$histogramOut <- renderPlotly({
+    # numSize stores all "Varying with device" sizes as 0.
+    # This does not work with histograms.
+    histData <- dataDf %>% filter(numSize != 0)
 
-    hist(dist) # Plot histogram with base R.
+    graph <- ggplot(histData, aes_string(x = input$histX)) +
+        geom_histogram(binwidth = input$bins, fill = "cyan")
+
+
+    if (input$histFacet != "Don't facet") {
+      # Doesn't work unless wrapped histFacet is wrapped in a get() call.
+      graph <- graph + facet_wrap(~get(input$histFacet), scales = "free_y")
+    }
+
+    ggplotly(graph)
   })
+
+  # Graphs for the scatterplot page.
+  output$scatterOut <- renderPlotly({
+    graph <- ggplot(tmpData, aes_string(x = input$scatterX, y = input$scatterY, color = input$scatterColour)) +
+      geom_point() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+    ggplotly(graph)
+  })
+
+  output$barOut <- renderPlotly({
+    graph <- ggplot(dataDf, aes_string(x = input$barX, fill = input$barX)) +
+      geom_bar() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+    ggplotly(graph)
+  })
+
+  output$boxOut <- renderPlotly({
+    groupedData <- reactive(dataDf %>%
+    # group_by_ is not the same as group_by. Note the underscore at the end.
+    # The underscore version works with strings, which is what gets provided by Shiny
+    # as opposed to actual names. Therefore, this function converts the
+    # input string to a corresponding column of that name. 
+    group_by_(xaxis = input$boxX, yaxis = input$boxY))
+
+    graph <- ggplot(groupedData(), aes(x = xaxis, y = yaxis)) +
+      geom_boxplot(fill='skyblue') +
+      labs(x = input$boxX, y = input$boxY) +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+    ggplotly(graph)
+  })
+
 })
--- Assignment/RNW/Shiny/ui.R
+++ Assignment/RNW/Shiny/ui.R
@@ -1,28 +1,200 @@
 library(shiny)
+library(tidyverse)
+library(ggplot2)
+library(plotly)
 
 # Shiny requires a client-server setup, with a file for each.
 # This file declares the client's UI.
 
-# Because everything below is an argument to the
-# shinyUI(pageWithSidebar()) function, it must be comma-seperated.
+dataDf <- read.csv("C:/Users/Lewis/DataspellProjects/DataVis/Assignment/RNW/data/wrangledData.csv")
 
-shinyUI(pageWithSidebar(
-  # Title
-  headerPanel("Shiny example"),
+# This is split over 6 pages using navbarPage and tabPanels
 
-  # Sidebar, where users give input.
-  sidebarPanel(
-    sliderInput("obs",
-      "Number of observations",
-      min = 1,
-      max = 50,
-      value = 30
+shinyUI( 
+  navbarPage(title = "Data analysis of the Google Play Store dataset",
+  # INFO PAGE ---------------------------------------------------------------------------------
+  tabPanel("Info",
+  
+  p("This is the Shiny webapp to accompany my CMP5352 Assignment 2 report."), 
+
+  p("Within this web app, you can produce graphs from the dataset used within the report."),
+
+  p("The app is split over five 'tabPanels', as you can see at the top of this page.
+  Each one of these panels is a generator for different types of graph."),
+
+  p("The graphs shown are all generated with plotly, which means that you can use your mouse to hover
+  over data points, and also use functionalities like zooming in and panning around the graphs."),
+
+  a("The dataset used in the report can be found here.", href = "https://www.kaggle.com/datasets/lava18/google-play-store-apps/data")
+
+  ),
+  # HISTOGRAMS ----------------------------------------------------------------------------------------
+  tabPanel("Histograms",
+    
+    headerPanel("Histogram generator"),
+    
+
+    
+    # Sidebar, where users give input.
+    sidebarPanel(
+      selectInput(
+        inputId = "histX",
+        label = "X-axis:",
+        choices = c(
+          "Size" = "numSize",
+          "Review score" = "Rating"
+          
+          
+          ),
+        selected = "numSize"
+      ),
+      
+      selectInput(
+        inputId = "histFacet",
+        label = "Facet by:",
+        choices = c(
+          "Don't facet", 
+          "Install thresholds" = "installThresholds"
+        ),
+        selected = ""
+      ),
+      
+      sliderInput(
+        inputId = "bins",
+        label = "Bin width",
+        min = 0.1,
+        max = 50,
+        step = 0.1, # Ratings go in decimals.
+        value = 1
+        )
+      
+      
+    ),
+    
+    # Main panel to output a graph.
+    mainPanel(
+      plotlyOutput("histogramOut")
     )
   ),
+  # SCATTERS ---------------------------------------------------------------------------------
+  tabPanel("Scatterplots",
+    
+    headerPanel("Scatterplot generator"),
+    
 
-  # Main panel to output a graph.
-  mainPanel(
-    plotOutput("distPlot")
+    
+    # Sidebar, where users give input.
+    sidebarPanel(
+      selectInput(
+        inputId = "scatterX",
+        label = "X-axis:",
+        choices = c(
+          "Rating", "Reviews (under 4 million)" = "Reviews",
+          "Size" = "numSize",
+          "Required Android version" = "numVer"
+        ),
+        selected = "numSize"
+      ),
+      
+      selectInput(
+        inputId = "scatterY",
+        label = "Y-axis:",
+        choices =c(
+          "Rating", "Reviews (under 4 million)" = "Reviews",
+          "Size" = "numSize",
+          "Required Android version" = "numVer"
+        ),
+        selected = ""
+      ),
+
+      selectInput(
+        inputId = "scatterColour",
+        label = "Colour by:",
+        choices = c(
+          "Size thresholds" = "sizeThresholds",
+          "Install thresholds" = "installThresholds",
+          "Required Android version" = "Android.Ver",
+          "Type of app (Free, Paid)" = "Type",
+          "Category" # Doesn't need to be assigned to something else,
+          # because it's the name of the column as well as the selection.
+        ),
+        selected = "Type"
+      )
+      
+      
+    ),
+    
+    # Main panel to output a graph.
+    mainPanel(
+      plotlyOutput("scatterOut")
+    )
+  ),
+  # BARS ----------------------------------------------------------------------------------------------
+    tabPanel("Bar charts",
+    
+    headerPanel("Bar chart generator"),
+    
+
+    
+    # Sidebar, where users give input.
+    sidebarPanel(
+      selectInput(
+        inputId = "barX",
+        label = "X-axis:",
+        choices = c(
+          "Install thresholds" = "installThresholds",
+          "Size thresholds" = "sizeThresholds",
+          "Required Android version" = "numVer",
+          "Content rating" = "Content.Rating"
+        ),
+        selected = "numSize"
+      ),
+
+      
+    ),
+    
+    # Main panel to output a graph.
+    mainPanel(
+      plotlyOutput("barOut")
+    )
+  ),
+  # BOXPLOTS ----------------------------------------------------------------------
+    tabPanel("Boxplots",
+    
+    headerPanel("Boxplot generator"),
+    
+
+    
+    # Sidebar, where users give input.
+    sidebarPanel(
+      selectInput(
+        inputId = "boxX",
+        label = "X-axis:",
+        choices = c(
+          "Install thresholds" = "installThresholds",
+          "Size thresholds" = "sizeThresholds",
+          "Required Android version" = "numVer",
+          "Content rating" = "Content.Rating"
+        ),
+        selected = "numSize"
+      ),
+
+      selectInput(
+        inputId = "boxY",
+        label = "Y-axis",
+        choices = c(
+          "Average rating" = "Rating",
+          "Average size" = "numSize"
+        ),
+        selected = "Rating"
+      )
+    ),
+    
+    # Main panel to output a graph.
+      mainPanel(
+        plotlyOutput("boxOut")
+      )
+    ),
   )
-
-))
+ 
+)
